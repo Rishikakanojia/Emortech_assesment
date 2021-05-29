368ed053b40ae69fbbdb2f9e8c88e7f38f51589a
RISHIKA <66744819+Rishikakanojia@users.noreply.github.com>
2021-05-30 03:33:57 +0530

    done

---
+++ index.js
@@ -0,0 +1,53 @@
+ // function checkPassword(form) {
+ //                pwd = form.pwd.value;
+ //                repwd = form.repwd.value;
+  
+ //                // If password not entered
+ //                if (document.getElementById(pwd) == '')
+ //                    alert ("Please enter Password");
+                      
+ //                // If confirm password not entered
+ //                else if (repwd == '')
+ //                    alert ("Please enter confirm password");
+                      
+ //                // If Not same return False.    
+ //                else if (pwd != repwd) {
+ //  document.getElementById("Sec2").style.display = "none";
+ //                    return false;
+ //                }
+  
+ //                // If same return True.
+ //                else {
+ //  document.getElementById("Sec1").style.display = "none";
+ //                    return true;
+ //                }
+ //            }
+
+ const submit = () => {
+if(document.getElementById("pwd").value == "")
+document.getElementById("error").innerHTML= "please enter password";
+
+else if(document.getElementById("repwd").value == "")
+document.getElementById("error").innerHTML= "please enter password";
+
+ else if(document.getElementById("pwd").value != document.getElementById("repwd").value
+)
+document.getElementById("error").innerHTML="passswords don't match";
+ 
+  else if (document.getElementById("pwd").value.length< 8)
+document.getElementById("error").innerHTML="passswords is short";
+
+else {
+  document.getElementById("sec1").style.display = "none";
+  document.getElementById("sec2").style.display = "block";
+  
+
+
+                }
+            }
+
+
+
+
+
+
